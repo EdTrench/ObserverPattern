89bae30e4f7ccf7e04f7ad4860c488f5679e4b9b
Edward G Trenchard <ed_trench@yahoo.com>
2013-01-31 13:35:52 +0000

    31-01-2013

---
+++ TheObserverPattern/ISubject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheObserverPattern
+{
+    interface ISubject
+    {
+        void registerObserver(IObserver o);
+        void removeObserver(IObserver o);
+        void notifyObservers();
+    }
+}
---
+++ TheObserverPattern/WeatherData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheObserverPattern
+{
+    class WeatherData : ISubject
+    {
+        private List<IObserver> observers;
+        private float temperature;
+        private float humidity;
+        private float pressure;
+
+        public WeatherData()
+        {
+            observers = new List<IObserver>();
+        }
+
+        public void registerObserver(IObserver o)
+        {
+            observers.Add(o);
+        }
+
+        public void removeObserver(IObserver o)
+        {
+            observers.Remove(o);
+        }
+
+        public void notifyObservers()
+        {
+            foreach (IObserver o in observers)
+            {
+                o.update(temperature, humidity, pressure);
+            }
+        }
+
+        public void measurementsChanged()
+        {
+            notifyObservers();
+        }
+
+        public void setMeasurements(float temperature, float humidity, float pressure)
+        {
+            this.temperature = temperature;
+            this.humidity = humidity;
+            this.pressure = pressure;
+        }
+    }
+}
